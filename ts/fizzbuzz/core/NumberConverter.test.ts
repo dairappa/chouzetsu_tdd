34ab74671814944f0309190c752fda649abe7de1
dairappa <dairappa@gmail.com>
2024-09-01 21:28:57 +0000

    テストを落とす

--- ts/fizzbuzz/core/NumberConverter.test.ts
+++ ts/fizzbuzz/core/NumberConverter.test.ts
@@ -4,4 +4,5 @@ import { NumberConverter } from "./NumberConverter";
 test("test", () => {
 	const fizzbuzz = new NumberConverter();
 	expect(fizzbuzz.convert(1)).toBe("1");
+	expect(fizzbuzz.convert(2)).toBe("2");
 });
