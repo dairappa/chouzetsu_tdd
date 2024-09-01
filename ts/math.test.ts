480902470021f4ffe8bb9dc4cf8f15512dda0aef
dairappa <dairappa@gmail.com>
2024-09-01 00:56:41 +0000

    add detailed test

--- ts/math.test.ts
+++ ts/math.test.ts
@@ -1,14 +1,26 @@
 import { expect, test } from "bun:test";
 import { MyMath } from "./math";
 
-test("min max test", () => {
+test("min test", () => {
 	const math = new MyMath();
 
-	const minExpected = 1;
-	const minResult = math.min(1, 2);
-	expect(minResult).toBe(minExpected);
+	expect(math.min(0, 1)).toBe(0);
+	expect(math.min(1, 0)).toBe(0);
+	expect(math.min(0, -1)).toBe(-1);
+	expect(math.min(-1, 0)).toBe(-1);
+	expect(math.min(0, 0)).toBe(0);
+	expect(math.min(0, Number.MAX_SAFE_INTEGER)).toBe(0);
+	expect(math.min(Number.MIN_SAFE_INTEGER, 0)).toBe(Number.MIN_SAFE_INTEGER);
+});
 
-	const maxExpected = 2;
-	const maxResult = math.max(1, 2);
-	expect(maxResult).toBe(maxExpected);
+test("max test", () => {
+	const math = new MyMath();
+
+	expect(math.max(0, 1)).toBe(1);
+	expect(math.max(1, 0)).toBe(1);
+	expect(math.max(0, -1)).toBe(0);
+	expect(math.max(-1, 0)).toBe(0);
+	expect(math.max(0, 0)).toBe(0);
+	expect(math.max(0, Number.MAX_SAFE_INTEGER)).toBe(Number.MAX_SAFE_INTEGER);
+	expect(math.max(Number.MIN_SAFE_INTEGER, 0)).toBe(0);
 });
