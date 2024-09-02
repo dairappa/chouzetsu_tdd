c66f10a5ab7980cb71306ccc01a69411e9b69e7c
dairappa <dairappa@gmail.com>
2024-09-02 21:12:41 +0000

    設計を導くTDD

--- ts/fizzbuzz/core/NumberConverter.test.ts
+++ ts/fizzbuzz/core/NumberConverter.test.ts
@@ -2,11 +2,6 @@ import { test, expect } from "bun:test";
 import { NumberConverter } from "./NumberConverter";
 
 test("test", () => {
-	const fizzbuzz = new NumberConverter();
-	expect(fizzbuzz.convert(1)).toBe("1");
-	expect(fizzbuzz.convert(2)).toBe("2");
-	expect(fizzbuzz.convert(3)).toBe("Fizz");
-	expect(fizzbuzz.convert(4)).toBe("4");
-	expect(fizzbuzz.convert(5)).toBe("Buzz");
-	expect(fizzbuzz.convert(15)).toBe("FizzBuzz");
+	const fizzbuzz = new NumberConverter([]);
+	expect(fizzbuzz.convert(1)).toBe("");
 });
--- ts/fizzbuzz/core/NumberConverter.ts
+++ ts/fizzbuzz/core/NumberConverter.ts
@@ -1,18 +1,11 @@
+export interface ReplaceRuleInterface {
+	replace: (input: number) => string;
+}
+
 export class NumberConverter {
+	constructor(rules: ReplaceRuleInterface[]) {}
+
 	convert(input: number): string {
-		const isDivisibleBy3 = input % 3 === 0;
-		const isDivisibleBy5 = input % 5 === 0;
-
-		if (isDivisibleBy3 && isDivisibleBy5) {
-			return "FizzBuzz";
-		}
-		if (isDivisibleBy3) {
-			return "Fizz";
-		}
-		if (isDivisibleBy5) {
-			return "Buzz";
-		}
-
-		return input.toString();
+		return "";
 	}
 }
