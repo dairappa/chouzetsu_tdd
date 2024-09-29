b421b8a871a8c17e18ccec4b75191f7c23006057
dairappa <dairappa@gmail.com>
2024-09-29 21:05:58 +0000

    refactor

--- ts/fizzbuzz/core/NumberConverter.ts
+++ ts/fizzbuzz/core/NumberConverter.ts
@@ -6,12 +6,7 @@ export class NumberConverter {
 	constructor(private rules: ReplaceRuleInterface[]) {}
 
 	convert(input: number): string {
-		for (const rule of this.rules) {
-			if (rule.replace(input)) {
-				return rule.replace(input);
-			}
-		}
-
-		return "";
+		const result = this.rules.find(rule => rule.replace(input));
+		return result ? result.replace(input) : "";
 	}
 }
