7ce27ab71447ad89ec267d99ac20b74471acae54
dairappa <dairappa@gmail.com>
2024-10-01 20:51:46 +0000

    add fizzbuzz rule (failed)

--- ts/fizzbuzz/core/NumberConverter.test.ts
+++ ts/fizzbuzz/core/NumberConverter.test.ts
@@ -1,6 +1,12 @@
 import { test, expect, spyOn } from "bun:test";
 import { NumberConverter, type ReplaceRuleInterface } from "./NumberConverter";
 
+class TestRule implements ReplaceRuleInterface {
+	replace(number: number): string {
+		return "";
+	}
+}
+
 test("empty rule", () => {
 	const fizzbuzz = new NumberConverter([]);
 	expect(fizzbuzz.convert(1)).toBe("");
@@ -20,11 +26,7 @@ test("single rule", () => {
 });
 
 test("convert with multiple rules", () => {
-	class TestRule implements ReplaceRuleInterface {
-		replace(number: number): string {
-			return "";
-		}
-	}
+
 
 
 	const fizzRule = new TestRule();
@@ -41,4 +43,22 @@ test("convert with multiple rules", () => {
 
 	expect(fizzbuzz.convert(1)).toBe("fizzbuzz");
 	
+});
+
+test("fizz buzz rule", () => {
+	const fizzRule = new TestRule();
+	spyOn(fizzRule, "replace").mockImplementation((i) => i === 3 ? "fizz" : "");
+
+	const buzzRule = new TestRule();
+	spyOn(buzzRule, "replace").mockImplementation((i) => i === 5 ? "buzz" : "");
+
+	const passThroughRule = new TestRule();
+	spyOn(passThroughRule, "replace").mockImplementation((i) => i.toString());
+
+	const fizzbuzz = new NumberConverter([fizzRule, buzzRule, passThroughRule]);
+
+	expect(fizzbuzz.convert(3)).toBe("fizz");
+	expect(fizzbuzz.convert(5)).toBe("buzz");
+	expect(fizzbuzz.convert(15)).toBe("fizzbuzz");
+	expect(fizzbuzz.convert(1)).toBe("1");
 });
