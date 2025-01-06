6e9869a13b99685f38cba49b2971a83934bfeef3
dairappa <dairappa@gmail.com>
2025-01-06 20:39:40 +0000

    echorテストを追加

--- rs/echor/tests/cli.rs
+++ rs/echor/tests/cli.rs
@@ -2,18 +2,24 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
+// C#などだとBoxingは値型をポインタ参照で渡すこと（参照渡しとすること）、Unboxingはポインタ参照を元の値型に戻すこと
+// RustにもHeap領域とStack領域があり、Stack領域にはメモリ領域が固定されたもの = 値型 or 参照ポインタ が入ることになる。
+// dyn は文字通り動的に解決されること（この場合はError traitを実装したものに動的に解決される）を意味する。
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.assert()
+fn dies_no_args() -> TestResult {
+    Command::cargo_bin("echor")?
+        .assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello").assert().success();
+fn runs() -> TestResult {
+    Command::cargo_bin("echor")?.arg("hello").assert().success();
+    Ok(())
 }
 
 #[test]
